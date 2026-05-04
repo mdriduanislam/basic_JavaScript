eca3f2f76e42ad77a785b7a29fbfc654d26fdef6
mdriduanislam <mohhammadris786@gmail.com>
2026-05-04 23:48:01 +0600

    reversed vowels in string

--- index.js
+++ index.js
@@ -800,3 +800,23 @@
 // console.log(productExceptSelf([1,2,3,4]));
 
 
+var reverseVowels = function(s) {
+    let arr = s.split('');
+    let left = 0;
+    let right = arr.length - 1;
+    let vowels = "aeiouAEIOU";
+    while (left < right) {
+        if (!vowels.includes(arr[left])) {
+            left++;
+        } 
+        else if (!vowels.includes(arr[right])) {
+            right--;
+        } 
+        else {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    return arr.join('');
+};
