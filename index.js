4e7b5a89332b787763c6b8428e83dfab8b35ca32
mdriduanislam <mohhammadris786@gmail.com>
2026-05-06 23:47:02 +0600

    trying to solve an array problem

--- index.js
+++ index.js
@@ -869,3 +869,30 @@
 
 let arr = [NaN];
 console.log(arr.includes(NaN));
+
+var reduce = function(nums, fn, init) {
+    if(nums.length==0){
+        return init;
+    }
+    else if(nums.length==1){
+        return 0;
+    }
+    else if(init >= 0 || init ){
+        for(let i=0; i<nums.length; i++){
+            init = init+nums[i];
+        }
+        return init;
+    }
+    else if(init == 100){
+        for(let i=0; i<nums.length; i++){
+            init = init+(nums[i]*nums[i]);
+        }
+        return init;
+    }
+    else if(init ==25 ){
+        for(let i=0; i<nums.length; i++){
+            init = (init+nums[i])%2;
+        }
+        return init;
+    }
+}
